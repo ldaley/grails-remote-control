87207166f81c7d508678a3c68b76d82c824c94c3
Luke Daley <ld@ldaley.com>
2011-01-29 14:13:40 +1000

    Version 1.1.1 - fixes problems with badly declared dependencies.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -11,6 +11,18 @@ grails.project.dependency.resolution = {
 		mavenCentral()
 		mavenRepo "https://nexus.codehaus.org/content/repositories/releases"
 		
+		
+	}
+	dependencies {
 		compile "org.codehaus.groovy.modules.remote:remote-transport-http:0.2"
+	}
+	plugins {
+		compile (
+			":tomcat:$grailsVersion",
+			":hibernate:$grailsVersion",
+			":spock:0.5-groovy-1.7"
+		) {
+			export = false
+		}
 	}
 }
